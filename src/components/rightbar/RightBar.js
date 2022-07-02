0cd1407a5c1794fdd035bffd59514dac2a3f29ff
LAKSHYA <llakshya63@gmail.com>
2022-07-02 18:06:42 +0530

    right bar is created with showing of online friends

--- src/components/rightbar/RightBar.js
+++ src/components/rightbar/RightBar.js
@@ -3,8 +3,66 @@ import "./RightBar.css"
 
 export default function RightBar() {
   return (
-    <div className='rightBarContainer'>
-      this is the rightbar
+    <div className='rightbarContainer'>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className='birthdayGift' src="/assets/gift.png" alt="giftCard" />
+          <span className='birthdayText'><b>Lakshya</b> and other 2 friends have birthday today.</span>
+        </div>
+        <img className='rightbarAd' src="/assets/ad.jpg" alt="Advertisement" />
+        <h4 className='rightbarTitle'>Online Friends</h4>
+        <ul className="onlineFriendsList">
+          <li className="onlineFriend">
+            <div className="onlineFriendProfileContainer">
+              <img src="/assets/persons/1.jpg" alt="" className="onlineFriendProfileImg" />
+              <span className='rightbarOnlineIcon'></span>
+            </div>
+            <span className="onlineFriendProfile">Jenny</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendProfileContainer">
+              <img src="/assets/persons/1.jpg" alt="" className="onlineFriendProfileImg" />
+              <span className='rightbarOnlineIcon'></span>
+            </div>
+            <span className="onlineFriendProfile">Jenny</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendProfileContainer">
+              <img src="/assets/persons/1.jpg" alt="" className="onlineFriendProfileImg" />
+              <span className='rightbarOnlineIcon'></span>
+            </div>
+            <span className="onlineFriendProfile">Jenny</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendProfileContainer">
+              <img src="/assets/persons/1.jpg" alt="" className="onlineFriendProfileImg" />
+              <span className='rightbarOnlineIcon'></span>
+            </div>
+            <span className="onlineFriendProfile">Jenny</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendProfileContainer">
+              <img src="/assets/persons/1.jpg" alt="" className="onlineFriendProfileImg" />
+              <span className='rightbarOnlineIcon'></span>
+            </div>
+            <span className="onlineFriendProfile">Jenny</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendProfileContainer">
+              <img src="/assets/persons/1.jpg" alt="" className="onlineFriendProfileImg" />
+              <span className='rightbarOnlineIcon'></span>
+            </div>
+            <span className="onlineFriendProfile">Jenny</span>
+          </li>
+          <li className="onlineFriend">
+            <div className="onlineFriendProfileContainer">
+              <img src="/assets/persons/1.jpg" alt="" className="onlineFriendProfileImg" />
+              <span className='rightbarOnlineIcon'></span>
+            </div>
+            <span className="onlineFriendProfile">Jenny</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
