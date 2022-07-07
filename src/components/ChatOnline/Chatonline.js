30eb62e6d0a074092954e163b19e26f6772d87d9
LAKSHYA <llakshya63@gmail.com>
2022-07-07 18:52:51 +0530

    chat engine ui is completed

---
+++ src/components/ChatOnline/Chatonline.js
@@ -0,0 +1,15 @@
+import "./chatonline.css"
+
+export default function Chatonline() {
+    return (
+        <div className="chatOnline">
+            <div className="chatOnlineFriend">
+                <div className="chatOnlineImgContainer">
+                    <img className="chatOnlineImg" src="https://apollo-singapore.akamaized.net/v1/files/xja2yo2z5rn83-IN/image;s=300x600;q=60" alt="" />
+                    <div className="chatOnlineBadge"></div>
+                </div>
+                <span className="chatOnlineName">Lakshya</span>
+            </div>
+        </div>
+    )
+}
--- src/pages/Messanger/Messanger.js
+++ src/pages/Messanger/Messanger.js
@@ -1,7 +1,8 @@
 import "./messanger.css"
 import Navbar from "../../components/Navbar/Navbar"
 import Conversation from "../../components/conversations/Conversation"
-import Message from "../../components/message/Message"
+import Message from "../../components/message/Message";
+import Chatonline from "../../components/ChatOnline/Chatonline";
 
 export default function messanger() {
     return (
@@ -24,17 +25,32 @@ export default function messanger() {
                             <Message />
                             <Message own={true} />
                             <Message />
-                            <Message own ={true} />
+                            <Message own={true} />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
+                            <Message />
                             <Message />
                         </div>
                         <div className="chatBoxBottom">
+                            <textarea className="chatMessageInput" placeholder="Write something...">
 
+                            </textarea>
+                            <button className="chatSubmitButton">
+                                Send
+                            </button>
                         </div>
                     </div>
                 </div>
                 <div className="chatOnline">
                     <div className="chatOnlineWrapper">
-                        online
+                        <Chatonline/>
                     </div>
                 </div>
             </div>
